c5c72eec91b75e11a55eb74226de4dd2bdb9d424
rov <valadares.thel@gmail.com>
2018-04-03 20:32:03 -0300

    Adds camera and galery image picker

--- meme_me_ios/ViewController.swift
+++ meme_me_ios/ViewController.swift
@@ -1,25 +1,40 @@
-//
-//  ViewController.swift
-//  meme_me_ios
-//
-//  Created by Raoni Valadares on 03/04/18.
-//  Copyright © 2018 solo. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+	@IBOutlet weak private var imageView: UIImageView!
+	@IBOutlet weak private var cameraButton: UIBarButtonItem!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		
+		cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
 	}
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	@IBAction func pickAnImageActionHandler(_ sender: UIBarButtonItem) {
+		let pickerController = UIImagePickerController()
+		pickerController.delegate = self
+		pickerController.sourceType = .photoLibrary
+		present(pickerController, animated: true, completion: nil)
 	}
-
-
+	
+	@IBAction func takeAPictureActionHandler(_ sender: Any) {
+		
+		let pickerController = UIImagePickerController()
+		pickerController.delegate = self
+		pickerController.sourceType = .camera
+		present(pickerController, animated: true, completion: nil)
+	}
 }
 
+extension ViewController: UIImagePickerControllerDelegate {
+	func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+		if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+			imageView.image = image
+		}
+		dismiss(animated: true, completion: nil)
+	}
+}
+
+extension ViewController: UINavigationControllerDelegate {
+	
+}
