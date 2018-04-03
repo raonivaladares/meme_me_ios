e2ac8c0c7c8c4470524736a831141b5545a1af33
rov <valadares.thel@gmail.com>
2018-04-03 19:19:12 -0300

    first commit

---
+++ meme_me_ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  meme_me_ios
+//
+//  Created by Raoni Valadares on 03/04/18.
+//  Copyright © 2018 solo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
