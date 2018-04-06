93e3bf5356fe3de3887b74ce5fee4cd3cfed953f
rov <valadares.thel@gmail.com>
2018-04-05 22:48:11 -0300

    Adds custom textfields and keyboard selectors

--- meme_me_ios/ViewController.swift
+++ meme_me_ios/ViewController.swift
@@ -1,15 +1,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-	@IBOutlet weak private var imageView: UIImageView!
-	@IBOutlet weak private var cameraButton: UIBarButtonItem!
+	@IBOutlet private weak var imageView: UIImageView!
+	@IBOutlet private weak var topTextField: UITextField!
+	@IBOutlet private weak var bottomTextField: UITextField!
+	@IBOutlet private weak var cameraButton: UIBarButtonItem!
+	
+	private let defaultTopText = "TOP"
+	private let defaultBottomText = "BOTTOM"
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
 		cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+		setupTextFields()
+		
+		
+	}
+	
+	// MARK: - View life-cycle
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(true)
+		
+		NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: .UIKeyboardWillShow, object: nil)
+		NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: .UIKeyboardWillHide, object: nil)
+	}
+	
+	override func viewWillDisappear(_ animated: Bool) {
+		NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
+		NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
 	}
 
+	// MARK: - Action handlers
 	@IBAction func pickAnImageActionHandler(_ sender: UIBarButtonItem) {
 		let pickerController = UIImagePickerController()
 		pickerController.delegate = self
@@ -24,8 +46,46 @@ class ViewController: UIViewController {
 		pickerController.sourceType = .camera
 		present(pickerController, animated: true, completion: nil)
 	}
+	
+	// MARK: - Selectors
+	@objc func keyboardWillShow(_ notification: Notification) {
+		if bottomTextField.isFirstResponder {
+			view.frame.origin.y = -getKeyboardHeight(notification)
+		}
+	}
+	
+	@objc func keyboardWillHide(_ notification: Notification) {
+		if bottomTextField.isFirstResponder {
+			view.frame.origin.y = 0
+		}
+	}
+	
+	// MARK: - Private methods
+	private func getKeyboardHeight(_ notification: Notification) -> CGFloat {
+		let userInfo = notification.userInfo
+		let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
+		return keyboardSize.cgRectValue.height
+	}
+	
+	private func setupTextFields() {
+		let memeTextAttributes:[String: Any] = [
+			NSAttributedStringKey.strokeColor.rawValue: UIColor.black,
+			NSAttributedStringKey.foregroundColor.rawValue: UIColor.white,
+			NSAttributedStringKey.font.rawValue: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+			NSAttributedStringKey.strokeWidth.rawValue: -3
+		]
+		
+		topTextField.defaultTextAttributes = memeTextAttributes
+		topTextField.delegate = self
+		bottomTextField.defaultTextAttributes = memeTextAttributes
+		bottomTextField.delegate = self
+		
+		topTextField.textAlignment = .center
+		bottomTextField.textAlignment = .center
+	}
 }
 
+// MARK: - Extensions
 extension ViewController: UIImagePickerControllerDelegate {
 	func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
 		if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
@@ -35,6 +95,21 @@ extension ViewController: UIImagePickerControllerDelegate {
 	}
 }
 
-extension ViewController: UINavigationControllerDelegate {
+extension ViewController: UINavigationControllerDelegate {}
+
+extension ViewController: UITextFieldDelegate {
+	func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+		if let text = textField.text,
+			text.contains(defaultTopText) || text.contains(defaultBottomText) {
+				textField.text = nil
+		}
+		
+		return true
+	}
 	
+	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+		textField.resignFirstResponder()
+		
+		return true
+	}
 }
